e0e8ceba31f543d7cb5dcacf7654a362f8ed84d0
Jonathan <Jonathan@DESKTOP-5LVATAU>
2017-10-13 17:16:59 -0700

    Add project files.

---
+++ WashCountyPropETL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WashCountyPropETL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
